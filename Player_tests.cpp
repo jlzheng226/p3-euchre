29da295ed0774c39373dedc6308dd9f9958f708d
jlzheng226 <123140450+jlzheng226@users.noreply.github.com>
2023-02-15 21:26:52 -0500

    Add files via upload

--- Player_tests.cpp
+++ Player_tests.cpp
@@ -1,6 +1,8 @@
 // Project UID 1d9f47bfc76643019cfbf037641defe1
 
 #include "Player.h"
+#include "Card.h"
+#include <sstream>
 #include "unit_test_framework.h"
 
 #include <iostream>
@@ -60,8 +62,97 @@ TEST(test_player_make_trump){
     delete alice;
     delete bob;
     delete Dan;
-
 }
+
+TEST(add_and_discard_lead_card){
+    Player* alice = Player_factory("Alice", "Simple");
+    Player* bob = Player_factory("Bob", "Simple");
+
+    const Card nine_diamonds = Card(NINE, DIAMONDS);
+    const Card king_diamonds = Card(KING, DIAMONDS);
+    const Card jack_hearts = Card(JACK, HEARTS);
+    const Card ten_spades = Card(TEN, SPADES);
+    const Card ace_hearts = Card(ACE, HEARTS);
+    const Card ten_diamonds = Card(TEN, DIAMONDS);
+    const Card queen_diamonds = Card(QUEEN, DIAMONDS);
+    Card upcard_a = Card(TEN, DIAMONDS);
+    Card upcard_b = Card(ACE, DIAMONDS);
+    Suit trump = DIAMONDS;
+
+    (*alice).add_card(nine_diamonds);
+    (*alice).add_card(king_diamonds);
+    (*alice).add_card(jack_hearts);
+    (*alice).add_card(ten_spades);
+    (*alice).add_card(ace_hearts);
+    (*alice).add_and_discard(upcard_a);
+    ASSERT_EQUAL((*alice).lead_card(trump), ace_hearts);
+    ASSERT_EQUAL((*alice).lead_card(trump), jack_hearts);
+    ASSERT_EQUAL((*alice).lead_card(trump), king_diamonds);
+    ASSERT_EQUAL((*alice).lead_card(trump), upcard_a);
+    ASSERT_EQUAL((*alice).lead_card(trump), nine_diamonds);
+    delete alice;
+
+    (*bob).add_card(ten_diamonds);
+    (*bob).add_card(king_diamonds);
+    (*bob).add_card(jack_hearts);
+    (*bob).add_card(queen_diamonds);
+    (*bob).add_card(nine_diamonds);
+    (*bob).add_and_discard(upcard_b);
+    ASSERT_EQUAL((*bob).lead_card(trump), jack_hearts);
+    ASSERT_EQUAL((*bob).lead_card(trump), upcard_b);
+    ASSERT_EQUAL((*bob).lead_card(trump), king_diamonds);
+    ASSERT_EQUAL((*bob).lead_card(trump), queen_diamonds);
+    ASSERT_EQUAL((*bob).lead_card(trump), ten_diamonds);
+    delete bob;
+}
+
+TEST(play_cards){
+    Player* alice = Player_factory("Alice", "Simple");
+    Player* bob = Player_factory("Bob", "Simple");
+    const Card nine_diamonds = Card(NINE, DIAMONDS);
+    const Card king_diamonds = Card(KING, DIAMONDS);
+    const Card jack_hearts = Card(JACK, HEARTS);
+    const Card ten_spades = Card(TEN, SPADES);
+    const Card king_clubs = Card(KING, CLUBS);
+
+    Card led_card_a = Card(TEN, DIAMONDS);
+    Card led_card_b = Card(KING, SPADES);
+    Suit trump = DIAMONDS;
+
+    (*alice).add_card(nine_diamonds);
+    (*alice).add_card(king_diamonds);
+    (*alice).add_card(jack_hearts);
+    (*alice).add_card(ten_spades);
+    (*alice).add_card(king_clubs);
+
+    (*bob).add_card(nine_diamonds);
+    (*bob).add_card(king_diamonds);
+    (*bob).add_card(jack_hearts);
+    (*bob).add_card(ten_spades);
+    (*bob).add_card(king_clubs);
+
+    Card play_card = (*alice).play_card(led_card_a, trump);
+    ASSERT_EQUAL(play_card, jack_hearts);
+    play_card = (*alice).play_card(led_card_a, trump);
+    ASSERT_EQUAL(play_card, king_diamonds);
+    play_card = (*alice).play_card(led_card_a, trump);
+    ASSERT_EQUAL(play_card, nine_diamonds);
+    play_card = (*alice).play_card(led_card_a, trump);
+    ASSERT_EQUAL(play_card, ten_spades);
+    play_card = (*alice).play_card(led_card_a, trump);
+    ASSERT_EQUAL(play_card, king_clubs);
+    delete alice;
+
+    play_card = (*bob).play_card(led_card_b, trump);
+    ASSERT_EQUAL(play_card, ten_spades);
+    ASSERT_EQUAL(play_card, king_clubs);
+    ASSERT_EQUAL(play_card, nine_diamonds);
+    ASSERT_EQUAL(play_card, king_diamonds);
+    ASSERT_EQUAL(play_card, jack_hearts);
+    delete bob;
+    
+}
+
 
 
 
